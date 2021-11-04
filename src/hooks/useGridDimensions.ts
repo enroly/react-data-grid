4d51880e885e83d04b400270ddc014080cab2476
Jonathan <jonnocraig@gmail.com>
2021-11-04 23:55:40 +0000

    width based on resized grid

--- src/hooks/useGridDimensions.ts
+++ src/hooks/useGridDimensions.ts
@@ -1,4 +1,4 @@
-import { useRef, useState, useLayoutEffect } from 'react';
+import { useRef, useState, useLayoutEffect, useCallback } from 'react';
 
 export function useGridDimensions(): [
   ref: React.RefObject<HTMLDivElement>,
@@ -9,24 +9,26 @@ export function useGridDimensions(): [
   const [gridWidth, setGridWidth] = useState(1);
   const [gridHeight, setGridHeight] = useState(1);
 
-  const handleResize = () => {
+  const handleResize = useCallback(() => {
     const { clientWidth, clientHeight } = gridRef.current!;
     // TODO: remove once fixed upstream
     // we reduce width by 1px here to avoid layout issues in Chrome
     // https://bugs.chromium.org/p/chromium/issues/detail?id=1206298
-    setGridWidth(clientWidth - (devicePixelRatio % 1 === 0 ? 0 : 1));
+    const viewPortWidth = clientWidth;
+    setGridWidth(viewPortWidth - (devicePixelRatio % 1 === 0 ? 0 : 1));
     setGridHeight(clientHeight);
-  };
+  }, [gridRef]);
 
   useLayoutEffect(() => {
     handleResize();
+
     window.addEventListener('resize', handleResize);
     window.addEventListener('maximize', handleResize);
     return () => {
       window.removeEventListener('resize', handleResize);
       window.removeEventListener('maximize', handleResize);
     };
-  }, []);
+  }, [handleResize]);
 
   return [gridRef, gridWidth, gridHeight];
 }
