1f5b77716306b262863939b37e50a80f2b1b32ee
Jonathan <jonnocraig@gmail.com>
2021-11-04 22:47:32 +0000

    handle resize

--- src/hooks/useGridDimensions.ts
+++ src/hooks/useGridDimensions.ts
@@ -10,12 +10,20 @@ export function useGridDimensions(): [
   const [gridHeight, setGridHeight] = useState(1);
 
   useLayoutEffect(() => {
-    const { clientWidth, clientHeight } = gridRef.current!;
-    // TODO: remove once fixed upstream
-    // we reduce width by 1px here to avoid layout issues in Chrome
-    // https://bugs.chromium.org/p/chromium/issues/detail?id=1206298
-    setGridWidth(clientWidth - (devicePixelRatio % 0.5 === 0 ? 0 : 1));
-    setGridHeight(clientHeight);
+    const handleResize = () => {
+      const { clientWidth, clientHeight } = gridRef.current!;
+      // TODO: remove once fixed upstream
+      // we reduce width by 1px here to avoid layout issues in Chrome
+      // https://bugs.chromium.org/p/chromium/issues/detail?id=1206298
+      setGridWidth(clientWidth - (devicePixelRatio % 1 === 0 ? 0 : 1));
+      setGridHeight(clientHeight);
+    };
+    window.addEventListener('resize', handleResize);
+    window.addEventListener('maximize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+      window.removeEventListener('maximize', handleResize);
+    };
   }, []);
 
   return [gridRef, gridWidth, gridHeight];
